d9d34633002197d9d7b1bdeec51fe0d3c490e237
Ruben <rubennykaas@gmail.com>
2020-06-17 14:57:57 +0200

    packages

---
+++ test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+import pandas as pd
+import dash
+import matplotlib.pyplot as plt
+
+
+print("Hello world")
+for i in range(0,10):
+    print(i)
+
+x = np.linspace(1,10,10)
+print(x)
+print(random.randrange(0,10))
+
+x = [0,1,2]
+y = [0,1,2]
+
+plt.plot([1, 2, 3, 4])
+plt.xlabel("X aksen")
+plt.ylabel('some numbers')
+plt.show()
